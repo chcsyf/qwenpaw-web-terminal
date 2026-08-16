082736db5774c8646e4e616b04b34d50267aea44
archigram <archigram@gitee.com>
2026-08-16 16:28:35 +0000

    sidebar menu icon: add qwenpaw-menu-item-icon class, unify font-size 16 (no version bump)

--- ui/index.js
+++ ui/index.js
@@ -1625,7 +1625,7 @@
         id: PLUGIN_ID + ".menu",
         location: "primary.settings",
         label: "终端",
-        icon: function () { return h("span", { style: { fontSize: 18 } }, "🖥️"); },
+        icon: function () { return h("span", { className: "qwenpaw-menu-item-icon", style: { fontSize: 16 } }, "🖥️"); },
         route: PLUGIN_ID + ".home",
         order: 80
       }]);
